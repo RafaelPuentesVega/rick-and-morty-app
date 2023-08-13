8c6bf93bc558a101e7637a52edb318011a066270
RafaelPuentes <61299147+RafaelPuentes@users.noreply.github.com>
2023-08-13 17:16:06 -0500

    ajuste img - declaracion variable environment

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnInit } from '@angular/core';
 import {HttpClient , HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 import { catchError, finalize } from 'rxjs/operators';
 
@@ -11,7 +12,7 @@ import { catchError, finalize } from 'rxjs/operators';
 export class ApiService {
   constructor(private http: HttpClient) { }
 
-  api_url = 'https://rickandmortyapi.com/api/';
+  api_url = environment.apiUrl;
 
   /**
  * GET Http Request
